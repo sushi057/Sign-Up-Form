7e97e60caed4203a9f5d219cc1eb6da4f9a0e28d
sushi057 <champsuvash@gmail.com>
2022-10-10 12:58:27 +0545

    Password Checking

---
+++ script.js
@@ -0,0 +1,26 @@
+const pwd1 = document.getElementById('password');
+const pwd2 = document.getElementById('confirm-password');
+const button = document.querySelector('button');
+const section = document.querySelector('#password-section');
+const message = document.createElement('p');
+message.textContent = '*Passwords do not match';
+message.style.color = "red";
+message.style.fontSize = "14px";
+
+button.addEventListener('click', () => {
+    console.log(pwd1);
+    console.log(pwd2)
+
+    if(pwd1.value !== pwd2.value){
+        pwd1.classList.add('error-input');
+        pwd2.classList.add('error-input');
+        section.appendChild(message);
+    }
+    else{
+        pwd1.classList = '';
+        pwd2.classList = '';
+        section.removeChild(message);
+    }
+})
+
+
